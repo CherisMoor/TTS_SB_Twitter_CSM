547f6e533a83c98f7169d8aabeb79d2bd6f5b4f1
Cherish <62115637+CherisMoor@users.noreply.github.com>
2020-07-28 09:14:20 -0400

    First push of TechTalentTwitter from slides. Models,Controllers,Repository/interfaces added

---
+++ src/main/java/com/tts/techtalenttwitter/service/UserService.java
@@ -0,0 +1,23 @@
+package com.tts.techtalenttwitter.service;
+
+import com.tts.techtalenttwitter.repository.RoleRepository;
+import com.tts.techtalenttwitter.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, RoleRepository roleRepository,
+            BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+}
